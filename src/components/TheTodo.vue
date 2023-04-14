0426d385d9ffe0abd4bc2d1d89d40f852694c946
IABED <imane.abed@ynov.com>
2023-04-14 17:03:23 +0200

    initial commit

---
+++ src/components/TheTodo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    Vue’s provides you with all information you need to get started.
+  </WelcomeItem>
+</template>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,39 @@
+<script setup>
+  let msg = "HOOOO";
+</script>
+
+
+<template>
+  <div>
+  <div class="todo">
+    <h1>Voici la todo list </h1>
+    <h1>CouCou</h1>
+    <span>{{ msg }}</span>
+    <h1>Salut</h1>
+  </div>
+  <label for="pet-select">Choose a pet:</label>
+
+<select name="pets" id="pet-select">
+    <option value="">--Please choose an option--</option>
+    <option value="dog">Dog</option>
+    <option value="cat">Cat</option>
+    <option value="hamster">Hamster</option>
+    <option value="parrot">Parrot</option>
+    <option value="spider">Spider</option>
+    <option value="goldfish">Goldfish</option>
+</select>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .todo {
+    min-height: 100vh;
+    align-items: center;
+  }
+}
+span {
+  color:red;
+}
+
+</style>
+
